534b7f020d0cccd9d4c61dc925c2635a41941bc7
pratikgajbhiye222 <pratik.gajbhiye222@gmail.com>
2020-07-13 00:34:07 +0530

    Initial Commit

---
+++ NewsApp-MVVMNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp-MVVMNew
+//
+//  Created by pratik gajbhiye on 13/07/20.
+//  Copyright © 2020 Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
